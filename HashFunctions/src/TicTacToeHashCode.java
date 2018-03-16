2e323d001b650ae8ae747b10bf9149350a7b6d92
Sean Gibbons <gibbonss@bishopireton.org>
2018-03-16 10:42:07 -0400

    inclass work

--- HashFunctions/src/TicTacToeHashCode.java
+++ HashFunctions/src/TicTacToeHashCode.java
@@ -1,28 +1,96 @@
-//TODO Make sure you remove all of the TODO comments from this file before turning itin
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * A class that creates
+ * 
+ * @author gibbonss
+ *
+ */
+// TODO Make sure you remove all of the TODO comments from this file before
+// turning itin
 
 public class TicTacToeHashCode extends Board {
 
-	boolean[] winners; // True if the hash string that maps to this index is a winner, false otherwise
+	boolean[] winners; // True if the hash string that maps to this index is a
+						// winner, false otherwise
 
 	TicTacToeHashCode(String s) {
 		super(s);
-		// TODO Instantiate/fill winners array.
+		winners = new boolean[TicTacToe.POSSIBILITIES];
+		for (int i = 0; i < winners.length; i++) {
+			winners[i] = false;// instantiates the winners array and fills it with all empty values
+		}
+
 	}
 
-	// TODO - write the myHashCode function. It must create a unique hashcode for
-	// all of the
-	// possible values the game board (3 ^ 9) and it MUST use the super.charAt(row,
-	// col) function
 	@Override
 	public int myHashCode() {
+		int result = 0;
 
-		return 0;
+		int powersOf3[][] = new int[][] { { 1, 3, 9 }, { 27, 81, 243 }, { 729, 2187, 6561 } };
+
+		for (int i = 0; i < TicTacToe.ROWS; i++) {
+			for (int j = 0; j < TicTacToe.COLS; j++) {
+				switch (charAt(i, j)) {
+				case 'x':
+					result += powersOf3[i][j];
+					break;
+				default:
+					result += 2 * powersOf3[i][j];
+					break;
+				}
+
+			}
+		}
+		return result;
+	}
+
+	public void fillWinners() {
+		Scanner w = fileToScanner("TicTacToeWinners.txt");
+		int i = 0;
+		while (w.hasNextLine()) {
+			String board = (w.nextLine());
+			if (TicTacToe.isWin(board)) {
+				winners[i++] = true;
+			}
+		}
+	}
+
+	/**
+	 * Converts the given file to Scanner
+	 * 
+	 * @param fName
+	 *            The String name of a file
+	 * 
+	 * @return A Scanner of the file with the given file name
+	 */
+	private static Scanner fileToScanner(String fName) {
+
+		File fileName = new File(fName);
+		Scanner words = null;
+
+		try {
+			words = new Scanner(fileName);
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to Open file: " + fName + ", please see output");
+			return null;
+
+		}
+		if (!words.hasNext())
+			throw new IllegalArgumentException("File is empty");
+		return words;
+
 	}
 
 	public boolean isWin(String s) {
-		// return the value in the winner array for the hash chode of the board string
-		// sent in.
-		return true;
+		fillWinners();
+		if (winners[this.myHashCode()]) {
+			return true;
+
+		}
+		return false;
 	}
 
 	public static void main(String[] args) throws InterruptedException {
@@ -33,7 +101,7 @@ public class TicTacToeHashCode extends Board {
 			board.show(currentBoard);
 			board.setHashCode(board.myHashCode());
 			// TODO Update this line to call your isWin method.
-			board.setWinner(TicTacToe.isWin(currentBoard));
+			board.setWinner(board.isWin(currentBoard));
 
 			Thread.sleep(4000);
 		}
